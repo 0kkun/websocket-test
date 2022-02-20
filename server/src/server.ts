342352800a91149a7e2655d711de64f1b197fc77
0kkun <shinji.okumoto.01@gmail.com>
2022-02-21 01:30:15 +0900

    接続維持用のping処理実装

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,27 +2,49 @@ import express from 'express';
 import ws from 'ws';
 import WebSocket from 'ws';
 
+// 環境変数読み込み
 const env = process.env
-const WEBSOCKET_PORT = 3000
+const WEBSOCKET_PORT: number = Number(env.NODE_PORT) || 3000;
 
+// ログ設定
 const log4js = require('log4js')
 log4js.configure('./src/config/local/log4js.config.json')
 const logger = log4js.getLogger('access')
 
+// サーバー起動
 const server = new ws.Server({port: WEBSOCKET_PORT})
 logger.info(`Socket server is running... [ENV:${env.NODE_ENV}]`)
+
+// 接続コントロール用
+let connectedCount: number = 0;
+let connections = [];
+
+// 疎通を確認する間隔
+const PING_INTERVAL_SEC = 10;
 
 /**
  * Websocket connection
  */
 server.on('connection', ws => {
     try {
-        console.log(process.env.NODE_ENV)
+        logger.info(`Running PORT : ${WEBSOCKET_PORT}`)
 
-            /* message event */
-    ws.on('message', (message: string) => {
-        console.log('receive message from client!')
-    })
+        /** 疎通確認を一定間隔で行う (接続維持) */
+        let interval = setInterval(() => {
+            if (ws.bufferedAmount == 0) {
+                ws.ping();
+            } else {
+                ws.close();
+                logger.info("Close socket because ping couldn't be sent.")
+            }
+        }, 1000 * PING_INTERVAL_SEC);
+
+        /* message event */
+        ws.on('message', (message: string) => {
+            logger.info('Received a message from the client.');
+            const request = JSON.parse(message);
+            
+        })
 
     } catch (exception) {
         console.error(exception)
