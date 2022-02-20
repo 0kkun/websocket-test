8db36964b01c6dab513ee33115fb086f81cfcec5
0kkun <shinji.okumoto.01@gmail.com>
2022-02-21 01:12:47 +0900

    ログ出力実装

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,26 +2,29 @@ import express from 'express';
 import ws from 'ws';
 import WebSocket from 'ws';
 
-const WEBSOCKET_PORT = 3000;
+const env = process.env
+const WEBSOCKET_PORT = 3000
 
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-const server = new ws.Server({port: WEBSOCKET_PORT});
+const log4js = require('log4js')
+log4js.configure('./src/config/local/log4js.config.json')
+const logger = log4js.getLogger('access')
+
+const server = new ws.Server({port: WEBSOCKET_PORT})
+logger.info(`Socket server is running... [ENV:${env.NODE_ENV}]`)
 
 /**
  * Websocket connection
  */
 server.on('connection', ws => {
     try {
-        console.log(process.env.NODE_ENV);
+        console.log(process.env.NODE_ENV)
 
             /* message event */
     ws.on('message', (message: string) => {
-        console.log('receive message from client!');
+        console.log('receive message from client!')
     })
 
     } catch (exception) {
-        console.error(exception);
+        console.error(exception)
     }
 });
